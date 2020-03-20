b901f6f5f2d1390d7c96c7cf3232c815eb930d75
Brianna Workman <brianna.workman@hotmail.com>
2020-03-20 10:50:35 -0500

    Completes count_th

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,8 +3,17 @@ Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
+
+
 def count_th(word):
-    
-    # TBC
-    
-    pass
+    # Base case: if the length of the word is less than 2, we can assume the string "th" isn't in the word and can return 0
+    # Working towards the base case:
+    # if the string between indexes 0 and 2 equal "th", add 1 to the cout
+    # if not, continue to the next set of indexes
+
+    if len(word) < 2:
+        return 0
+    elif word[0:2] == "th":
+        return 1 + count_th(word[2:])
+    else:
+        return count_th(word[1:])
